1a68e24e1bba2aafd9e78d32771038ea328ded10
Ryo Novoa <ryosnovoa@gmail.com>
2024-10-08 12:21:51 -0700

    reconfigured error handling for project_routes

--- routes/project_routes.py
+++ routes/project_routes.py
@@ -57,27 +57,34 @@ def get_project(id):
   connection = get_db_connection()
   if connection:
     cursor = connection.cursor()
-    # Structure query, retrieve project
-    query = "SELECT * FROM projects WHERE id = %s AND username = %s"
     try:
-      cursor.execute(query, (id, username))
+      query_a = "SELECT * FROM projects WHERE id = %s"
+      cursor.execute(query_a, (id,))
       project = cursor.fetchone()
-      if project:
-        project_data = {
-          "id": project[0], 
-          "username": project[1], 
-          "title": project[2],
-          "summary": project[3],
-          # Convert JSON strings to list format
-          "steps": json.loads(project[4]),
-          "languages": json.loads(project[5]),
-          "status": project[6]
-        }
-        # 200 OK: For a successful request that returns data
-        return jsonify(project_data), 200
-      else:
-        # 404 Not Found: Projects not found
-        return jsonify({"error": f"No project found with ID {id} under user {username}"}), 404
+      if not project:
+        # 404 Not Found: Project not found
+        return jsonify({"error": f"No project found with ID {id}"}), 404
+      
+      # Structure query, retrieve project
+      query_b = "SELECT * FROM projects WHERE id = %s AND username = %s"
+      cursor.execute(query_b, (id, username))
+      project = cursor.fetchone()
+      if not project:
+        # 403 Forbidden: Project exists, but does not belong to the user
+        return jsonify({"error": f"Project ID {id} does not belong to user {username}"}), 403
+
+      project_data = {
+        "id": project[0], 
+        "username": project[1], 
+        "title": project[2],
+        "summary": project[3],
+        # Convert JSON strings to list format
+        "steps": json.loads(project[4]),
+        "languages": json.loads(project[5]),
+        "status": project[6]
+      }
+      # 200 OK: For a successful request that returns data
+      return jsonify(project_data), 200
     except mysql.connector.Error as e:
       # 500 Internal Server Error: Generic server-side failures
       return jsonify({"error": str(e)}), 500
@@ -144,10 +151,16 @@ def create_project():
   connection = get_db_connection()
   if connection:
     cursor = connection.cursor()
-    query_a = "INSERT INTO projects (username, title, summary, steps, languages, status) VALUES (%s, %s, %s, %s, %s, %s)"
     try:
+      query_a = "SELECT * FROM users WHERE username = %s"
+      cursor.execute(query_a, (username,))
+      user = cursor.fetchone()
+      if user[5] != user[6]:
+        # 409 Conflict: User-side error in request
+        return jsonify({"error": f"User {username} already has project in progress. User must complete existing project before creating a new one"}), 409
+      query_b = "INSERT INTO projects (username, title, summary, steps, languages, status) VALUES (%s, %s, %s, %s, %s, %s)"
       # Convert 'steps' list to JSON string for storage
-      cursor.execute(query_a, (username, title, summary, json.dumps(steps), json.dumps(languages), 0))
+      cursor.execute(query_b, (username, title, summary, json.dumps(steps), json.dumps(languages), 0))
       # Retrieve last inserted project ID (pid)
       pid = cursor.lastrowid
       # Commit changes
@@ -155,17 +168,17 @@ def create_project():
       # Generate tasks lists for each project step
       tasks_lists = prompt_ai_to_generate_tasks(engineer_taskgen_prompt(title, summary, languages, steps))
       # Structure task insertion query
-      query_b = "INSERT INTO tasks (pid, description, priority, status) VALUES (%s, %s, %s, %s)"
+      query_c = "INSERT INTO tasks (pid, description, priority, status) VALUES (%s, %s, %s, %s)"
       # Enumerate starting from 1 to extract priority based on step number
       for priority, tasks_list in enumerate(tasks_lists, start=1):
         # Get tasks_list for each step, defaulting to empty list if not found
         for task in tasks_list.get('tasks', []):
           # Execute query. Status is default 1 to indicate it is "to-do"
-          cursor.execute(query_b, (pid, task, priority, 1))
+          cursor.execute(query_c, (pid, task, priority, 1))
       # Commit changes
       connection.commit()
-      query_c = "UPDATE users SET projects = projects + 1 WHERE username = %s"
-      cursor.execute(query_c, (username,))
+      query_d = "UPDATE users SET projects = projects + 1 WHERE username = %s"
+      cursor.execute(query_d, (username,))
       # Commit changes
       connection.commit()
       response = jsonify({"message": "Project, tasks creation successful"})
@@ -184,7 +197,7 @@ def create_project():
   return jsonify({"error": "Failed to connect to database"}), 500
 
 # UPDATE
-# Updates status of a given project
+# Toggle status of a given project
 # TODO: Add check that this is indeed a project in username's username
 @project_bp.route('/project/<int:id>/update-status', methods=['PUT'])
 @jwt_required()
@@ -193,33 +206,42 @@ def update_project_status(id):
   connection = get_db_connection()
   if connection:
     cursor = connection.cursor()
-    query_a = "SELECT * FROM projects WHERE id = %s"
     try:
+      # Check if project exists
+      query_a = "SELECT * FROM projects WHERE id = %s"
       cursor.execute(query_a, (id,))
       project = cursor.fetchone()
-      if project:
-        project_status = project[6]
-        print(f"Project ID {id} current status: {project_status}")
-        # Prepare queries
-        if project_status == 0:
-          new_status = 1
-          query_c = "UPDATE users SET projects_completed = projects_completed + 1 WHERE username = %s"
-        else:
-          new_status = 0
-          query_c = "UPDATE users SET projects_completed = projects_completed - 1 WHERE username = %s"
-
-        # Update project status
-        query_b = "UPDATE projects SET status = %s WHERE id = %s"
-        cursor.execute(query_b, (new_status, id))
-        # Update user's project completion count
-        cursor.execute(query_c, (username,))    
-        # Commit both changes together
-        connection.commit()
-        # 200 OK: For a successful request
-        return jsonify({"message": "Project status updated successfully"}), 200
+      if not project:
+        # 404 Not Found: Project not found
+        return jsonify({"error": f"No project found with ID {id}"}), 404
+      
+      # Check if existing project lists current user as owner
+      query_b = "SELECT * FROM projects WHERE id = %s AND username = %s"
+      cursor.execute(query_b, (id, username))
+      project = cursor.fetchone()
+      if not project:
+        # 403 Forbidden: Project exists, but does not belong to the user
+        return jsonify({"error": f"Project ID {id} does not belong to user {username}"}), 403
+      
+      project_status = project[6]
+      print(f"Project ID {id} current status: {project_status}")
+      # Prepare queries to update user's project completion count
+      if project_status == 0:
+        new_status = 1
+        query_d = "UPDATE users SET projects_completed = projects_completed + 1 WHERE username = %s"
       else:
-        # 404 Not Found: User not found
-        return jsonify({"error": "Project not found"}), 404
+        new_status = 0
+        query_d = "UPDATE users SET projects_completed = projects_completed - 1 WHERE username = %s"
+
+      # Update project status
+      query_c = "UPDATE projects SET status = %s WHERE id = %s"
+      cursor.execute(query_c, (new_status, id))
+      # Update user's project completion count
+      cursor.execute(query_d, (username,))    
+      # Commit both changes together
+      connection.commit()
+      # 200 OK: For a successful request
+      return jsonify({"message": "Project status updated successfully"}), 200
     except mysql.connector.Error as e:
       # 500 Internal Server Error
       return jsonify({"error": f"Database error: {e}"}), 500
@@ -237,23 +259,33 @@ def delete_project(id):
   username = get_jwt_identity()
   connection = get_db_connection()
   if connection:
+    cursor = connection.cursor()
     try:
-      cursor = connection.cursor()
-      query_a = "SELECT * FROM projects WHERE id = %s AND username = %s"
-      cursor.execute(query_a, (id, username))
+      # Check if project exists
+      query_a = "SELECT * FROM projects WHERE id = %s"
+      cursor.execute(query_a, (id,))
       project = cursor.fetchone()
-      if project:
-        query_b = "DELETE FROM tasks where pid = %s"
-        cursor.execute(query_b, (id,))
-        query_c = "DELETE FROM projects WHERE id = %s"
-        cursor.execute(query_c, (id,))
-        # Commit changes
-        connection.commit()
-        # 200 OK: For a successful request
-        return jsonify({"message": "Project deleted successfully."}), 200
-      else:
+      if not project:
         # 404 Not Found: Project not found
-        return jsonify({"error": "Project not found."}), 404
+        return jsonify({"error": f"No project found with ID {id}"}), 404
+      
+      query_b = "SELECT * FROM projects WHERE id = %s AND username = %s"
+      cursor.execute(query_b, (id, username))
+      project = cursor.fetchone()
+      if not project:
+        # 403 Forbidden: Project exists, but does not belong to the user
+        return jsonify({"error": f"Project ID {id} does not belong to user {username}"}), 403
+      
+      # Delete tasks first, as they are linked to project through pid 
+      query_c = "DELETE FROM tasks where pid = %s"
+      cursor.execute(query_c, (id,))
+      # Delete project
+      query_d = "DELETE FROM projects WHERE id = %s"
+      cursor.execute(query_d, (id,))
+      # Commit changes
+      connection.commit()
+      # 200 OK: For a successful request
+      return jsonify({"message": "Project deleted successfully."}), 200
     except mysql.connector.Error as e:
       # 500 Internal Server Error
       return jsonify({"error": f"Database error: {e}"}), 500
